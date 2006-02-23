b604859e08540714b5f7209f81e8993ad1f99435
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-02-23 16:42:03 +0000

    Updated vote form formatting Updated the footer to look more professional Added date handling
    git-svn-id: https://source.sakaiproject.org/www/trunk@45 a679bb99-f00c-0410-8349-afe10950bba1

--- Public/html/requirements/tool_vars.php
+++ Public/html/requirements/tool_vars.php
@@ -14,7 +14,9 @@ $JIRA_REQ = "http://bugs.sakaiproject.org/jira/browse/";
 
 // these control the round of voting
 $ROUND = 1;
-$ROUND_DATE_FORMAT = "YYYY/MM/DD HH24:MI";
+// date format for display
+$ROUND_DATE_FORMAT = "l, F dS, Y h:i A";
+// dates below in format: YYYY/MM/DD HH24:MI
 $ROUND_OPEN_DATE = "2006/02/24 17:00";
 $ROUND_SWITCH_DATE = "2006/03/06 1:00";
 $ROUND_CLOSE_DATE = "2006/03/14 1:00";
