883af97a5ffad06a881c5cd55f0246b6ba31c540
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-02-23 20:00:19 +0000

    updated for testing
    git-svn-id: https://source.sakaiproject.org/www/trunk@53 a679bb99-f00c-0410-8349-afe10950bba1

--- Public/html/requirements/new_db.sql
+++ Public/html/requirements/new_db.sql
@@ -13,8 +13,6 @@ CREATE TABLE requirements_data (
 
 // Import a JIRA export saved as a CSV file
 //mysqlimport -c jirakey,summary,component,description,audience --fields-optionally-enclosed-by=""" --fields-terminated-by=, --lines-terminated-by="\r\n" --local -usakaiwww -p sakaiweb requirements_data.csv
-// if needed: alter table requirements_data add jiranum int(5) not null default 0
-// if needed: update requirements_data set jiranum = replace(jirakey,'REQ-','')
 
 CREATE TABLE requirements_vote ( 
     pk	         	int(10) AUTO_INCREMENT NOT NULL,
@@ -28,10 +26,13 @@ CREATE TABLE requirements_vote (
     FOREIGN KEY (req_data_pk) REFERENCES requirements_data(pk)
 );
 
-/**** to get old tables to line up with new structure
+/**** to get old tables to line up with new structure (use if needed)
 alter table requirements_data drop column toolname;
 alter table requirements_data drop column need;
 alter table requirements_data drop column timeframe;
 alter table requirements_data add round int(4) NOT NULL default 1;
 alter table requirements_vote add round int(4) NOT NULL default 1;
+alter table requirements_vote add official enum('0','1') NOT NULL DEFAULT '0'
+alter table requirements_data add jiranum int(5) not null default 0
+update requirements_data set jiranum = replace(jirakey,'REQ-','')
 *****/
