92a364ec7f308f332d0f4fa28a220dfe9596b2ce
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-02-28 13:45:04 +0000

    Fixed email validation to handle subnets
    git-svn-id: https://source.sakaiproject.org/www/trunk@80 a679bb99-f00c-0410-8349-afe10950bba1

--- Public/html/accounts/myaccount.php
+++ Public/html/accounts/myaccount.php
@@ -86,7 +86,7 @@ function focus(){document.account.firstname.focus();}
 		}
 
 		// verify that the email address is valid
-		if(!eregi('^[a-zA-Z0-9._-]+@[a-zA-Z0-9-]+\.[a-zA-Z.]{2,5}$', $EMAIL)) {
+		if(!eregi(' ^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$', $EMAIL)) {
 			$Message .= "<span class='error'>Error: You have entered an invalid email address!</span><br/>";
 			$errors++;
 	        unset($EMAIL);
