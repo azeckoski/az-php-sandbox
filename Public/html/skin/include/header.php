b30f1894c0ec606670cb1a358034f3d394961157
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-10 18:41:15 +0000

    adding skin project
    git-svn-id: https://source.sakaiproject.org/www/trunk@140 a679bb99-f00c-0410-8349-afe10950bba1

---
+++ Public/html/skin/include/header.php
@@ -0,0 +1,41 @@
+</head>
+<body>
+<table class=main border="0" >
+<tr>
+  <td width=120px style="border: 1px solid #ccc;">
+	<a href="http://www.sakaiproject.org"><img src="http://www.sakaiproject.org/images/stories/conferenceLogos/logoslate160x89.jpg" height=59 width=110  border=0></a>
+  </td>
+  <td width="100%" valign=top style="background:#ECECEC;border: 1px solid #ccc;">
+
+	<table width="100%" border="0">
+	<tr>
+	<td width="90%" valign="top">
+	  	<span style="font-size:1.3em;font-weight:bold;"><?= $SYSTEM_NAME ?> <?= $TOOL_NAME ?></span>
+<?php if (isset($PAGE_NAME)) { ?>
+	  	<span style="font-size:1.1em;font-weight:bold;"> - <?= $PAGE_NAME ?></span>
+<?php } ?>
+	<br/>
+	<a style="font-size:.8em;" href="index.php">Main Page</a> -
+	<a style="font-size:.8em;" href="vote.php">Voting Form</a> -
+	<a style="font-size:.8em;" href="results.php">View Results</a>
+  	<?= $EXTRA_LINKS ?>
+	</td>
+	<td width="10%" align="right" valign="top">
+<?php if ($USER_PK > 0) { ?>
+	<span style="font-size:.9em;">
+  	Welcome,&nbsp;<?= $USER["firstname"] ?>&nbsp;<?= $USER["lastname"] ?>
+  	</span><br/>
+  	<a style="font-size:.8em;" href="<?= $ACCOUNTS_URL ?>/<?= $ACCOUNTS_PAGE ?>">My Account</a><br/>
+<?php if ($USER["admin_reqs"]) { ?>
+  	<a style="font-size:.8em;" href="<?= $TOOL_PATH ?>/admin.php">Tool Admin</a> - 
+<?php } ?>
+  	<a style="font-size:.8em;" href="<?= $ACCOUNTS_URL ?>/<?= $LOGOUT_PAGE ?>">Logout</a><br/>
+<?php } else { ?>
+  	<a style="font-size:.8em;" href="<?= $ACCOUNTS_URL ?>/<?= $LOGIN_PAGE."?ref=".$_SERVER['PHP_SELF'] ?>">Login</a>
+<?php } ?>
+	</td>
+	</tr>
+	</table>
+  </td>
+</tr>
+</table>
---
+++ Public/html/skin/include/help.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * Created on Febrary 18, 2006 by @author aaronz
+ * Aaron Zeckoski (aaronz@vt.edu) - Virginia Tech (http://www.vt.edu/)
+ */
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title><?= $TOOL_NAME ?> - Help</title>
+<link href="requirements_vote.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<!-- Definition of terms -->
+<div class="definitions">
+<div class="defheader">General Help</div>
+<div class="padded">
+
+<UL>
+<LI>Votes can be changed at any time during the voting period (see the top of the voting form for dates).
+<LI>Once votes have been saved, they cannot be removed. Please make sure you are happy with your votes before saving them.
+<LI>The Save button will save all vote selections.
+<LI>The Reset button will only reset the vote for a single item.
+</UL>
+
+</div>
+</div>
+
+<?php include 'defineterms.html'; ?>
+
+<input type="button" value="Close Help Window" onClick="javascript:window.close();">
+
+</body>
+</html>
