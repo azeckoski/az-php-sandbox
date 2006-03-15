05921d11aa5241733e00ae0cec4d5c270629cbc4
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-15 14:37:30 +0000

    Updated the payment processing code
    git-svn-id: https://source.sakaiproject.org/www/trunk@204 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/registration/includes/verisignSilent_post.php
+++ Private/html/conference/registration/includes/verisignSilent_post.php
@@ -1,179 +1,50 @@
 <?php
 // Gather Silent Post data with this PHP script.
-// use $_POST instead of $HTTP_POST_VARS for version PHP 4.0 or greater.
-  
- if (!empty($HTTP_POST_VARS)) {
-  extract($HTTP_POST_VARS);
-
-    }
-    
-     if (!$_POST) {
-
- // No post received
- echo "No Post received\n";
- 
- }
- 
-     if ($_POST) {
-     
-     echo "completed post";
-
- // Close the file
- //fclose ($fp);
-
-
-
- // Print out post contents
- 
-  // $last4cc=$_POST['USER2'];
+if ($_POST) {
+	echo "completed post";
+	// Print out post contents
+	// $last4cc=$_POST['USER2'];
     $user_id=$_POST['USER1'];
-
-  $registrant=$_POST['USER2'];
-  $co_registrant=$_POST['USER3'];
-  $transID=$_POST['PNREF'];
-  $transAmount=$_POST['AMOUNT'];
-
-
-$ResultCode=$_POST['RESULT'];
-$ResponseMsg=$_POST['RESPMSG'];
-
- $CSCMATCH=$_POST['CSCMATCH'];
- 
-$AVSDATA=$_POST['AVSDATA'];
-
-
+	$registrant=$_POST['USER2'];
+	$co_registrant=$_POST['USER3'];
+	$transID=$_POST['PNREF'];
+	$transAmount=$_POST['AMOUNT'];
+	$ResultCode=$_POST['RESULT'];
+	$ResponseMsg=$_POST['RESPMSG'];
+	$CSCMATCH=$_POST['CSCMATCH'];
+	$AVSDATA=$_POST['AVSDATA'];
     
-  // Loop through post array
-//  foreach ($_POST as $key => $value) {
-    
-//echo "Variable: $key; Value: $value\n\r\r<br />";
+	// Loop through post array
+	//  foreach ($_POST as $key => $value) {
+	//echo "Variable: $key; Value: $value\n\r\r<br />";
 //  }
   
-  
-  //set variables for updating the registration data
-    
+//set variables for updating the registration data
 
-if ($ResultCode>0){
-include('index.php');  //error received so 
+	// TODO - What was this supposed to do?
+	if ($ResultCode > 0 || !$USER_PK){
+		//include('index.php');  //error received so 
+		// kick them back to the payment if this fails, probably not a good way to handle it
+		$PAYMENT_MSG = "?msg=".urlencode("Failure during payment processing");
+		header('location:'.$TOOL_PATH.'/registration'.$PAYMENT_MSG);
+		exit;
+	}
+
+	//if ($ResponseMsg='CSCDECLINED'){
+	//include('index.php');
+	//}
+	//if ($ResponseMsg='AVSDECLINED'){
+	//include('index.php');
+	//}
+
+	if ($ResultCode== 0) {
+		// sql should be loaded already
+		
+		//no errors so update the conf table and activate this registration
+		$sql = "UPDATE sakaiConf_all SET datea = NOW(), " .
+			"fee='$transAmount', transID = '$transID', activated='1' " .
+			"WHERE users_pk='$USER_PK' and confID='$CONF_ID'"; 
+		$result = mysql_query($sql); 
+	}
 }
-
-//if ($ResponseMsg='CSCDECLINED'){
-//include('index.php');
-//}
-//if ($ResponseMsg='AVSDECLINED'){
-//include('index.php');
-//}
-
-
-
-
-if ($ResultCode== 0){ 
-
-//no errors
-    
-   require('../includes/mysqlconnect.php');
- $sql = "UPDATE sakaiconf_vancouver_ccard SET 
- datea = NOW(),
- fee='$transAmount',
- transID = '$transID' 
- WHERE id = '$user_id' LIMIT 1"; 
- 
- 
- $result = mysql_query($sql); 
- if ($result==1) {
-//copy this information to the conference database
-
-		$sql="SELECT * FROM sakaiconf_vancouver_ccard where id='$user_id'";
-		$result= mysql_query($sql);
-
-		while($links=mysql_fetch_array($result))
-{
-$firstname=$links["firstname"];
-$lastname=$links["lastname"];
-$badge=$links["badge"];
-$email=$links["email"];
-$institution=$links["institution"];
-$otherInst=$links["otherInst"];
-$dept=$links["dept"];
-$address1=$links["address1"];
-$address2=$links["address2"];
-$city=$links["city"];
-$state=$links["state"];
-$otherstate=$links["otherState"];
-$zip=$links["zip"];
-$country=$links["country"];
-$phone=$links["phone"];
-$fax=$links["fax"];
-$shirt=$links["shirt"];
-$spec=$links["special"];
-$hotel=$links["hotelInfo"];
-$jasig=$links["jasig"];
-$ospi=$links["ospi"];
-$contact=$links["contactInfo"];
-$fee=$links["fee"];
-$title=$links["title"];
-
-		}
-		
-		
-$register="INSERT INTO sakaiConf_all VALUES (
-		'',
-		NOW( ) ,
-		'June06',
-		'$firstname',
-		'$lastname', 
-		'$badge',
-		'$email',
-		'$institution',
-		'$otherInst',
-		'$dept',
-		'$address1',
-		'$address2',
-		'$city',
-		'$state',
-		'$otherState',
-		'$zip',
-		'$country',
-		'$phone',
-		'$fax',
-		'$shirt',
-		'$special',
-		'$hotel',
-		'$contact',
-		'$jasig',
-		'$ospi',
-		'$fee',
-		'$title'
-)";
-
-
-$result = mysql_query($register);
-
-
-//now provide a confirmation page		
-		 session_start();
-		 
-		 
-		 
-		 
-		 
-
-//include('index.html');
-
-//echo "done";
-         //                        include('email_confirmation.php');
-
-                                
- }
- 
-
- }
- }
- 
-
-  
- 
- 
-
-
 ?>
--- Private/html/conference/registration/payment.php
+++ Private/html/conference/registration/payment.php
@@ -11,6 +11,23 @@ require '../sql/mysqlconnect.php';
 // check authentication
 require $ACCOUNTS_PATH.'include/check_authentic.php';
 
+// login if not autheticated
+$AUTH_MESSAGE = "You must login to make payment for the $CONF_NAME conference. If you do not have an account, please create one and register first.";
+require $ACCOUNTS_PATH.'include/auth_login_redirect.php';
+
+// bring in inst and conf data
+require 'includes/getInstConf.php';
+
+// kick them off this page if they should not be here
+if ( (!$isRegistered) || $isPartner) {
+	// if not registered yet then send them packing
+	// if they are in a partner institution then also send them packing
+	$MSG = "?msg=".urlencode("Not registered or a partner " + $_GET['msg']);
+	header('location:'.$TOOL_PATH.'/registration/index.php'.$MSG);
+	exit;
+}
+$Message = $_GET['msg'];
+
 // add in the help link
 //$EXTRA_LINKS = " - <a style='font-size:9pt;' href='$HELP_LINK' target='_HELP'>Help</a><br/>";
 //$EXTRA_MESSAGE = "<br/><span style='font-size:8pt;'>Technical problems? Please contact <a href='mailto:$HELP_EMAIL'>$HELP_EMAIL</a></span><br/>";
@@ -26,94 +43,111 @@ padding: 0px 5px;
 color:#000;
 }
 </style>
-<script>
-<!--
-// -->
-</script>
+<script type="text/javascript" src="/accounts/ajax/validate.js"></script>
 <?php include '../include/header.php'; // INCLUDE THE HEADER ?>
 
 <?php
-
-$today = date("F j, Y"); 
-
+$today = date($DATE_FORMAT,time());
 
 /*******************************/
 //billing information
 /*******************************/
-if ($_SESSION['memberType']=="2"){
-//non member must pay
-
-if($_SESSION['jasig']=='Y'){
-
-   $amount='345.00';
-    }
-    else {
-    
-    $amount='395.00';
-    }
-    
-   
-  $_SESSION['fee']=$amount;
-  
-  }
-//echo $_SESSION['user_id']; 
-
-$user_id=$_SESSION['user_id'];
+$amount = '0.00';
+if (!$isPartner){
+	//non member must pay
+	if($CONF['jasig']=='Y'){
+		$amount='345.00';
+	} else {
+		$amount='395.00';
+	}
+	$_SESSION['fee']=$amount; // TODO - session?
+}
 ?>
+
 <table width="100%" class="blog" cellpadding="0" cellspacing="0">
   <tr>
     <td valign="top"><div class="componentheading">Sakai Conference Registration</div></td>
   </tr>
 </table>
+
+<?= $Message ?>
+
 <!-- start of the form td -->
 <div id=cfp> <br />
-  <!-- start form section -->
-  <form id=form1 method="POST" action="https://payments.verisign.com/payflowlink">
-    <table width="500px"  cellpadding="0" cellspacing="0">
-      <tr>
-        <td valign="top" colspan="2" style="padding:0px;"><span class="small"> * = Required fields</span> </td>
-      </tr>
-      <?php
-if ($message) {
-	echo "<tr><td colspan=2><div class=\"errors\" align=\"left\"><font color=red><strong>Please provide the following information:</strong></font>
-	<ul class=small style=\"padding:0px 10px;\">";	
-	foreach ($message as $key => $value) {
-		echo $value;	
-	}
-	echo "</ul></div></td></tr> ";
-}
 
- 
-  
-			echo"<tr><td valign=top><div align=\"right\"><strong>Registrant's Information: </strong></div></td>
-			<td>$_SESSION[firstname] $_SESSION[lastname]<br /> $_SESSION[email1]
-			<br /><br />$_SESSION[otherInst]<br />$_SESSION[address1]<br />";
-		
-			
-			echo"$_SESSION[city], $_SESSION[state], $_SESSION[zip]<br/>
-					$_SESSION[country] <br /><br />phone: $_SESSION[phone]<br />fax: $_SESSION[fax]</td>
-					</tr>";
-		
-		 echo"<tr><td valign=top><div align=\"right\"><strong><span>Registration Fee</span>: &nbsp;&nbsp;&nbsp;</strong> </div></strong> </div></td>
-			 <td width=300p>$ $_SESSION[fee]</td></tr>";
-		
-            $registrant=$_SESSION['firstname'] ." " . $_SESSION['lastname'];    
-            $co_registrant=$_SESSION['co_firstname'] ." " . $_SESSION['co_lastname'];
-        ?>
-      <tr>
-        <td>&nbsp;</td>
-        <td><br />
-          <input type="hidden" name="USER1" value="<?php echo $user_id ?>">
-          <input type="hidden" name="USER2" value="<?php echo $registrant ?>">
-          <input type="hidden" name="USER3" value="<?php echo $co_registrant ?>">
-          <!--  <input type="hidden" name="ORDERFORM" value="TRUE" >
-<input type="hidden" name="ECHODATA" value="TRUE" >
-<input type="hidden" name="EMAILCUSTOMER" value="FALSE" >
-<input type="hidden" name="SHOWCONFIRM" value="TRUE" >
--->
+<!-- start form section -->
+<form id='form1' method="POST" action="https://payments.verisign.com/payflowlink">
+<table width="500px"  cellpadding="0" cellspacing="0">
+	<tr>
+		<td valign="top" colspan="2" style="padding:0px;">
+			<span class="small"> * = Required fields</span>
+		</td>
+    </tr>
+	<tr>
+		<td valign=top>
+			<div align=\"right\">
+				<strong>Registrant's Information: </strong>
+			</div>
+		</td>
+		<td>
+			name: <?php echo $USER['firstname'] . " " . $USER['firstname']; ?><br /> 
+			email: <?php echo $USER['email']; ?><br/>
+			<br />
+			institution: 
+			<?php if ($CONF['otherInst']) {
+				echo $CONF['otherInst'];
+			} else {
+				echo $CONF['institution']; 
+			} ?><br/>
+			address:<br/>
+			<?php echo $USER['address']; ?><br />
+			<?php echo $USER['city'] ." ". $USER['state'] .", ". $USER['zipcode']; ?><br/>
+			<?php echo $USER['country']; ?><br />
+			<br />
+			phone: <?php echo $USER['phone']; ?><br />
+			fax: <?php echo $USER['fax']; ?><br/>
+		</td>
+	</tr>
+
+	<tr>
+		<td valign=top>
+			<div align=\"right\">
+				<strong><span>Registration Fee</span>: &nbsp;&nbsp;&nbsp;</strong> 
+			</div></strong>
+		</td>
+	 	<td width='300px'><?= $amount ?></td>
+	</tr>
+
+<?php if ($CONF['transID']) { 
+	// user has already paid
+?>
+
+	<tr>
+		<td valign=top>
+			<div align=\"right\">
+				<strong><span>Registration Fee</span>: &nbsp;&nbsp;&nbsp;</strong> 
+			</div></strong>
+		</td>
+	 	<td width='300px'><?= $amount ?></td>
+	</tr>
+
+<?php } else { 
+	// user needs to pay for this conference still
+?>
+	<tr>
+	    <td>&nbsp;</td>
+	    <td><br />
+			<input type="hidden" name="USER1" value="<?php echo $USER_PK ?>">
+			<input type="hidden" name="USER2" value="<?php echo $registrant ?>">
+			<input type="hidden" name="USER3" value="<?php echo $CONF['delegate'] ?>">
+			<!--  
+			<input type="hidden" name="ORDERFORM" value="TRUE" >
+			<input type="hidden" name="ECHODATA" value="TRUE" >
+			<input type="hidden" name="EMAILCUSTOMER" value="FALSE" >
+			<input type="hidden" name="SHOWCONFIRM" value="TRUE" >
+			-->
           <!-- cardnum for testing -->
           <input type="hidden" name="CARDNUM" value="4111111111111111" >
-          <!--  exp date for testing -->
           <!-- cardnum for testing -->
           <input type="hidden" name="EXPDATE" value="0806" >
           <!--  exp date for testing -->
@@ -121,9 +155,9 @@ if ($message) {
           <input type="hidden" name="TYPE" value="S">
           <input type="hidden" name="LOGIN" value="sakaiproject">
           <input type="hidden" name="PARTNER" value="verisign">
-          <?php  // $amount='1.00';
+<?php // $amount='1.00';
 ?>
-          <input type="hidden" name="AMOUNT" value="<?php echo $amount;?>">
+          <input type="hidden" name="AMOUNT" value="<?php echo $amount; ?>">
           <input type="hidden" name="DESCRIPTION" value="Sakai -Vancover Conference registration">
           <input type="submit" name="submit" value="pay by credit card >>" >
         </td>
@@ -133,5 +167,4 @@ if ($message) {
 </div>
 <!-- end cfp -->
 
-<?php  require_once('../include/footer.php'); 
-?>
+<?php require_once('../include/footer.php'); ?>
