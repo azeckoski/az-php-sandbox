6a457623314eddaa667e330586a4c99b18219a4a
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-19 21:05:15 +0000

    added cfp_vancouver_presentation table changes
    git-svn-id: https://source.sakaiproject.org/www/trunk@294 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/sql/new_db.sql
+++ Private/html/conference/sql/new_db.sql
@@ -44,3 +44,50 @@ alter table sakaiConf_all modify title varchar(50);
 alter table sakaiConf_all add payeeInfo text;
 alter table sakaiConf_all add transID varchar(100);
 alter table sakaiConf_all add expectations text;
+
+
+
+
+CREATE TABLE `cfp_vancouver_presentation` (
+  `id` int(11) NOT NULL,
+  `date` timestamp NULL default NULL,
+  `confID` varchar(10) NOT NULL default '',
+  `users_pk` int(10) NOT NULL default '0',
+  `topics` varchar(50) NOT NULL default '',
+  `p_format` varchar(90) NOT NULL default '0',
+  `p_title` varchar(100) NOT NULL default '',
+  `p_abstract` text NOT NULL,
+  `p_desc` text NOT NULL,
+  `p_speaker` varchar(200) NOT NULL default '',
+  `p_URL` varchar(150) NOT NULL default '',
+  `bio` text NOT NULL,
+  `firstname` varchar(100) NOT NULL default '',
+  `lastname` varchar(100) NOT NULL default '',
+  `email1` varchar(100) NOT NULL default '',
+  `dev` int(2) NOT NULL default '0',
+  `faculty` char(2) NOT NULL default '0',
+  `mgr` char(2) NOT NULL default '0',
+  `librarian` char(3) NOT NULL default '',
+  `sys_admin` char(2) NOT NULL default '0',
+  `univ_admin` char(2) NOT NULL default '0',
+  `ui_dev` char(2) NOT NULL default '0',
+  `support` char(2) NOT NULL default '',
+  `faculty_dev` char(2) NOT NULL default '0',
+  `implementors` char(2) NOT NULL default '0',
+  `instruct_dev` char(2) NOT NULL default '0',
+  `instruct_tech` char(3) NOT NULL default '',
+  `layout` varchar(10) NOT NULL default '',
+  `length` int(3) NOT NULL default '0',
+  `conflict_tues` char(2) NOT NULL default '0',
+  `conflict_wed` char(2) NOT NULL default '0',
+  `conflict_thurs` char(2) NOT NULL default '0',
+  `conflict_fri` char(2) NOT NULL default '0',
+  `co_speaker` varchar(250) NOT NULL default '',
+  `co_bio` text NOT NULL,
+  `approved` int(2) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+)
+
+
+//changes to cfp_vancouver_presentation
+ALTER TABLE `cfp_vancouver_presentation` CHANGE `p_track` `users_pk` INT( 10 ) NOT NULL DEFAULT '0';
