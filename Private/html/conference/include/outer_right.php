2e6ff57723f10bca25e05802c5036ddef441f452
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-21 16:17:42 +0000

    WEB-18 fixed
    git-svn-id: https://source.sakaiproject.org/www/trunk@346 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/include/outer_right.php
+++ Private/html/conference/include/outer_right.php
@@ -17,7 +17,7 @@
 	      <br />
 	      We look forward to seeing you in Vancouver, but we want to see you even before then --in the Sakai Conference Facebook. <br /><br /> Past conference attendees have greatly appreciated being able to put names to faces the moment they arrive at the conference, and often use the conference facebooks to help them locate someone they remember talking with at previous conferences. <br />
 	      <br />
-	      <strong>Please take the time now to <a href="/facebook/">add your photo to the facebook. </a><br />
+	      <strong>Please take the time now to <a href="/facebook/">add your photo to the facebook.</a><br />
 	      </strong> <br />
 	    </div>
 	</div> <!--end rightcol -->
