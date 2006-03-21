f4a24a378aa6d496eeab52ebde7dae04eee11aad
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-21 06:29:01 +0000

    tweaks to facebook structure
    git-svn-id: https://source.sakaiproject.org/www/trunk@334 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/facebook/sql/new_db.sql
+++ Private/html/facebook/sql/new_db.sql
@@ -2,9 +2,10 @@ CREATE TABLE facebook_entries (
     pk          		int(10) AUTO_INCREMENT NOT NULL,
     users_pk			int(10) NOT NULL,
     image_pk			int(10) NOT NULL,
-    date_created		timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+    date_created		timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_modified	timestamp NOT NULL,
     interests   		text NULL,
-    url    				varchar(255) NULL,
+    url    			varchar(255) NULL,
     approved			enum('0','1') NOT NULL DEFAULT '0',
     viewable			enum('0','1') NOT NULL DEFAULT '0',
     PRIMARY KEY(pk)
@@ -13,10 +14,10 @@ CREATE TABLE facebook_entries (
 // table for storing uploaded files
 CREATE TABLE facebook_images (
 	pk					INT(10) NOT NULL AUTO_INCREMENT,
-	date_created		timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-	name				VARCHAR(100) NOT NULL,
-	type				VARCHAR(100) NOT NULL,
-	size				INT NOT NULL,
+	date_created			timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	name					VARCHAR(100) NOT NULL,
+	type					VARCHAR(100) NOT NULL,
+	size					INT NOT NULL,
 	dimensions			VARCHAR(100) NOT NULL,
 	content				BLOB NOT NULL,
 	thumb				MEDIUMBLOB,
