b8bfd20fa0498aeaf0a27898de489e735e8509eb
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-23 20:21:01 +0000

    addding registration/admin files - just experimenting at this point
    git-svn-id: https://source.sakaiproject.org/www/trunk@376 a679bb99-f00c-0410-8349-afe10950bba1

---
+++ Private/html/conference/registration/include/tool_vars.php
@@ -0,0 +1,43 @@
+<?php
+
+// Bring in the system variables
+// You must verify the path to the system_vars and the
+// user control directory are correct or many things will break -AZ
+$ACCOUNTS_PATH = $_SERVER["DOCUMENT_ROOT"].'/accounts/';
+require ($ACCOUNTS_PATH.'include/system_vars.php');
+
+// Tool variables
+$TOOL_PATH = $REGISTER_ADMIN_URL;
+$TOOL_NAME = "Registration Reports";
+$TOOL_SHORT = "regist";
+$CSS_FILE = $REGISTER_URL."/include/accounts.css";
+//$HELP_LINK = "";
+
+$DATE_FORMAT = "l, F dS, Y h:i A";
+
+// LDAP variables
+$LDAP_SERVER = "reynolds.cc.vt.edu";
+//$LDAP_SERVER = "bluelaser.cc.vt.edu";
+$LDAP_PORT = "389";
+$LDAP_ADMIN_DN = "cn=Manager,dc=sakaiproject,dc=org";
+$LDAP_ADMIN_PW = "ldapadmin";
+$LDAP_READ_DN = "uid=0,ou=users,dc=sakaiproject,dc=org";
+$LDAP_READ_PW = "ironchef";
+
+
+// tool functions
+
+// Generate password, update db, and send email
+function makeRandomPassword() {
+      $salt = "abchefghjkmnpqrstuvwxyz0123456789";
+      srand((double)microtime()*1000000);
+      $i = 0;
+      while ($i <= 7) {
+            $num = rand() % 33;
+            $tmp = substr($salt, $num, 1);
+            $pass = $pass . $tmp;
+            $i++;
+      }
+      return $pass;
+}
+?>
