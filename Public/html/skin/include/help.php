f8f84701b39dfd44f71b917a3b39753d65e630cb
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-26 17:24:27 +0000

    Updated help text for skin submitter
    git-svn-id: https://source.sakaiproject.org/www/trunk@412 a679bb99-f00c-0410-8349-afe10950bba1

--- Public/html/skin/include/help.php
+++ Public/html/skin/include/help.php
@@ -15,7 +15,21 @@
 
 <!-- Definition of terms -->
 <div class="definitions">
-<div class="defheader">General Help</div>
+<div class="defheader">Submission Help</div>
+<div class="padded">
+
+<UL>
+<LI>The maximum size for a submitted file is 2 MB. If you have a skin file that is larger you will have to contact the Default Skin WG (<a href="../index.php">See main page</a>).
+<LI>We recommend you use the firefox extension: <a href="http://pearlcrescent.com/products/pagesaver/">Pearl Crescent Page Saver</a> for creating your screen captures.
+<LI>Screen captures should only include the browser window itself and should not include the entire screen or the browser header and footer.
+</UL>
+
+</div>
+</div>
+
+<!-- Definition of terms -->
+<div class="definitions">
+<div class="defheader">Voting Help</div>
 <div class="padded">
 
 <UL>
@@ -27,7 +41,6 @@
 
 </div>
 </div>
-<a class="pwhelp" href="mailto:<?= $HELP_EMAIL ?>">Email contact for assistance</a><br>
 
 <input type="button" value="Close Help Window" onClick="javascript:window.close();">
 
