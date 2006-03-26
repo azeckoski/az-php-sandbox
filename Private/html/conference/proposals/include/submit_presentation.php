8b023c4d1b8dc5f87a88fe8108dfadba0be68036
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-03-26 00:26:13 +0000

    changed submit_presentation code - using while stmt instead of for stmt to insert topic and audience data
    git-svn-id: https://source.sakaiproject.org/www/trunk@401 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/proposals/include/submit_presentation.php
+++ Private/html/conference/proposals/include/submit_presentation.php
@@ -4,22 +4,18 @@
 
 require_once('../sql/mysqlconnect.php');
 
-$title=addslashes($_POST['title']);
-$abstract=addslashes($_POST['abstract']);
-$desc=addslashes($_POST['desc']);
-$speaker=addslashes($_POST['speaker']);
-$bio=addslashes($_POST['bio']);
-$co_speaker=addslashes($_POST['co_speaker']);
-$co_bio=addslashes($_POST['co_bio']);
+$title=mysql_real_escape_string($_POST['title']);
+$abstract=mysql_real_escape_string($_POST['abstract']);
+$desc=mysql_real_escape_string($_POST['desc']);
+$speaker=mysql_real_escape_string($_POST['speaker']);
+$bio=mysql_real_escape_string($_POST['bio']);
+$co_speaker=mysql_real_escape_string($_POST['co_speaker']);
+$co_bio=mysql_real_escape_string($_POST['co_bio']);
 $url=$_POST['url'];
 
 $type=$_POST['type'];
 $layout=$_POST['layout'];
 $length=$_POST['length'];
-$conflict_tues=$_POST['conflict_tues'];
-$conflict_wed=$_POST['conflict_wed'];
-$conflict_thurs=$_POST['conflict_thurs'];
-$conflict_fri=$_POST['conflict_fri'];
 
 if ($_POST['conflict_tues']=='1'){
 	$conflict='Tues ';
@@ -34,7 +30,7 @@ if ($_POST['conflict_fri']=='1'){
 	$conflict .='Fri ';
 }
 
-  //add presentation information into conf_proposals --all except role and topic data
+  //add presentation information into conf_proposals --all data except role and topic data
 $presentation_sql="INSERT INTO `conf_proposals` ( `id` , `date_created` , `date_modified` , `confID` , `users_pk` , `type` , `new_type` , `title` , `abstract` , `desc` , `speaker` , `URL` , `bio` , `layout` , `length` , `conflict` , `co_speaker` , `co_bio` , `approved` )
 VALUES (
 '' , NOW(), '', '$CONF_ID', '$USER_PK', '$type', '' , '$title', '$abstract', '$desc' , '$speaker', '$url', '$bio' , '$layout' , '$length', '$conflict' , '$co_speaker' , '$co_bio' , 'N'
@@ -46,45 +42,46 @@ $result = mysql_query($presentation_sql) or die("Error:<br/>" . mysql_error() . 
 		"If you continue to have problems, please report the problem to the " .
 		"<a href='mailto:$HELP_EMAIL'>sakaiproject.org webmaster</a>." );
 		
-		$proposals_pk=mysql_insert_id(); //this is how to query the last entered auto-id entry
+		$proposals_pk=mysql_insert_id(); //get this proposal_pk
 
+//get the list of audience information submitted and enter them into the proposal_audiences table
+$audience_sql="select pk,role_name from roles order by role_order";
+$result = mysql_query($audience_sql) or die(mysql_error());
 
-
-//get all audience values and add to the database
-	for ($i = 1; $i <= 12; $i++) { //should be handled with an sql query in case list of roles increase
-	$audience="audience_" .$i;
-	if ($_POST[$audience] > 0){ // - only gets roles with values
-			$audience_level=$_POST[$audience];
+ while($audience_items=mysql_fetch_array($result)) {
+ 	$audience_pk=$audience_items['pk'];
+ 	$audienceID="audience_" .$audience_pk;
+ 	if ($_POST[$audienceID] > 0){ // - only get $audience_items with values above 0
+			$audience_choice=$_POST[$audienceID];
 			$insert_audience_sql="INSERT INTO `proposals_audiences` ( `pk` , `date_created` , `date_modified` , `proposals_pk` , `roles_pk` , `choice` )
-VALUES ('', NOW(), '', '$proposals_pk', '$i', '$audience_level')";
+VALUES ('', NOW(), '', '$proposals_pk', '$audience_pk', '$audience_choice')";
+
+	$audience_result = mysql_query($insert_audience_sql) or die("Error:<br/>" . mysql_error() . "problem entering role" );
 	
-		$audience_result = mysql_query($insert_audience_sql) or die("Error:<br/>" . mysql_error() . "problem entering role" );
-		
-		
-			}
-			
-			
-			
-		}
+ 	}
+ }
+
 	
-//get all topic values and add to the database
-	for ($i = 1; $i <= 28; $i++) { //should be handled with an sql query in case list of topics increase
-	$get_topic="topic_" .$i;
-	if ($_POST[$get_topic] > 0){ // - only gets roles with values
-			$topic_level=$_POST[$get_topic];
-	$insert_topic_sql="INSERT INTO `proposals_topics` ( `pk` , `date_created` , `date_modified` , `proposals_pk` , `topics_pk` , `choice` )
-	VALUES('', NOW(), '', '$proposals_pk', '$i', '$topic_level') " ;	
+//get topic information submitted and enter them into the proposal_topics table
+	$topic_sql="select pk, topic_name from topics order by topic_order";
+	$result = mysql_query($topic_sql) or die(mysql_error());
+
+	 while($topic_items=mysql_fetch_array($result)) {
+		$topic_pk=$topic_items['pk'];
+		$topicID="topic_" . $topic_pk;
+		if ($_POST[$topicID] > 0) { // - only get topics with values greater than 0
+			$topic_choice=$_POST[$topicID];
+		$insert_topic_sql="INSERT INTO `proposals_topics` ( `pk` , `date_created` , `date_modified` , `proposals_pk` , `topics_pk` , `choice` )
+		VALUES('', NOW(), '', '$proposals_pk', '$topicID', '$topic_choice') " ;	
 		
 		$topic_result = mysql_query($insert_topic_sql) or die("Error:<br/>" . mysql_error() . "problem entering topic" );
 		
 		
 			}
 			
-			
-			
-		}	
-			
-		
+ }
+ 
+ 
 		
 ?>
 
