1a3938e056542e608092350d255ab640a613732f
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-04-05 15:16:25 +0000

    AZ
    git-svn-id: https://source.sakaiproject.org/www/trunk@453 a679bb99-f00c-0410-8349-afe10950bba1

--- Public/html/offline.php
+++ Public/html/offline.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* @version $Id: offline.php,v 1.4 2005/01/06 01:13:28 eddieajau Exp $
+* @version $Id: offline.php,v 1.1 2005/07/22 01:57:42 eddieajau Exp $
 * @package Mambo
 * @copyright (C) 2000 - 2005 Miro International Pty Ltd
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
