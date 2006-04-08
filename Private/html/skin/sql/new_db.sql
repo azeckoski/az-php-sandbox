0acade55a1ab08dfa5b606983fee0f31df2784ef
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-04-08 15:19:03 +0000

    Ready for testing
    git-svn-id: https://source.sakaiproject.org/www/trunk@494 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/skin/sql/new_db.sql
+++ Private/html/skin/sql/new_db.sql
@@ -1,8 +1,8 @@
 // table for storing uploaded files and images
 CREATE TABLE skin_files (
 	pk					INT(10) NOT NULL AUTO_INCREMENT,
-    date_created		timestamp NULL default '0000-00-00 00:00:00',
     date_modified		timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_created		timestamp NULL,
 	name				VARCHAR(100) NOT NULL,
 	type				VARCHAR(100) NOT NULL,
 	size				INT NOT NULL,
@@ -17,8 +17,8 @@ CREATE TABLE skin_files (
 CREATE TABLE skin_entries (
 	pk					int(10) AUTO_INCREMENT NOT NULL,
 	users_pk			int(10) NOT NULL,
-    date_created		timestamp NULL default '0000-00-00 00:00:00',
     date_modified		timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_created		timestamp NULL,
     title				varchar(100),
     description			text,
     url					varchar(200),
@@ -37,11 +37,12 @@ CREATE TABLE skin_entries (
 // table for storing skin votes
 CREATE TABLE skin_vote (
 	pk					int(10) AUTO_INCREMENT NOT NULL,
-    date_created		timestamp NULL default '0000-00-00 00:00:00',
     date_modified		timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    date_created		timestamp NULL,
 	users_pk			int(10) NOT NULL,
 	skin_entries_pk		int(10) NOT NULL,
-	vote				int(2) NOT NULL,
+	vote_usability		int(2) NOT NULL,
+	vote_asthetic		int(2) NOT NULL,
 	round				int(4) NOT NULL default 1,
 	PRIMARY KEY(pk),
 	FOREIGN KEY (skin_entries_pk) REFERENCES skin_entries(pk)
