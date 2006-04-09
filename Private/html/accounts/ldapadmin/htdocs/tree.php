dedb833b7a2026a8747658e590cd5d1c333dd10d
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-04-09 02:19:09 +0000

    added libraries throughout ldapadmin
    git-svn-id: https://source.sakaiproject.org/www/trunk@560 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/accounts/ldapadmin/htdocs/tree.php
+++ Private/html/accounts/ldapadmin/htdocs/tree.php
@@ -18,6 +18,20 @@
 /**
  */
 
+// Have to load these modules here first -AZ
+if( !extension_loaded( 'gettext')) {
+    if( !@dl( 'gettext.so')) {
+		die("Could not enable the gettext library!");
+    }
+}
+
+// Load LDAP module also
+if (!extension_loaded('ldap')) {
+    if (!dl('ldap.so')) {
+    	die("Could not enable LDAP library!");
+    }
+}
+
 require './common.php';
 no_expire_header();
 
