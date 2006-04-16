0cfb9035d622992acaf3c8a71c9a0144cb1fa408
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-04-16 17:26:40 +0000

    Improvements to report generator script
    git-svn-id: https://source.sakaiproject.org/www/trunk@646 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/admin/conf_attendee_report.php
+++ Private/html/conference/admin/conf_attendee_report.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * file: conf_attendee_export.php
+ * file: conf_attendee_report.php
  * Created on Apr 15, 2006 by @author az
  * Aaron Zeckoski (aaronz@vt.edu) - Virginia Tech (http://www.vt.edu/)
  * copyright 2006 Virginia Tech
@@ -8,6 +8,16 @@
 ?>
 <?php
 require_once '../include/tool_vars.php';
+
+if (!$_REQUEST['gen']) {
+	header('location:'."/conference");
+	exit();
+} else if ($_REQUEST['gen'] != "file" && 
+	$_REQUEST['gen'] != "email" &&
+	$_REQUEST['gen'] != "both") {
+		echo "Invalid gen option: ".$_REQUEST['gen'];
+		exit();
+}
 
 require $ACCOUNTS_PATH.'sql/mysqlconnect.php';
 
@@ -22,10 +32,6 @@ $items_count = mysql_num_rows($result);
 
 $date = date("Ymd-Hi",time());
 $filename = "institutions-" . $date . ".csv";
-
-//$filePath = $ACCOUNTS_PATH."admin/conf_exports/";
-//$filename = $filePath.$filename;
-//$fhandle = fopen($filename,"w");
 
 ob_start(); // create buffer
 
@@ -48,41 +54,56 @@ echo ("\n\"Generated on:\",\"" . date($DATE_FORMAT,time()) . "\"\n");
 $fileContent = ob_get_contents(); // put buffer into variable
 ob_end_clean(); // purge the buffer
 
-//fclose($fhandle);
+// create a file if requested
+if($_REQUEST['gen'] == "file" || $_REQUEST['gen'] == "both") {
+	$filePath = $ACCOUNTS_PATH."admin/conf_exports/";
+	$filename = $filePath.$filename;
+	$fhandle = fopen($filename,"w");
+	if (!$fhandle) { echo "ERROR: Could not open file: $filename <br/>"; }
+	if (!fwrite($fileContent)) { echo "ERROR: Could not write contents to file<br/>"; }
+	fclose($fhandle);
+}
 
-// now mail this to someone
-ini_set(SMTP, $MAIL_SERVER);
-$headers  = 'From: ' . $HELP_EMAIL . "\n";
-$headers .= 'Return-Path: ' . $HELP_EMAIL . "\n";
-$headers .= 'Reply-To: ' . "donotreply@sakaiproject.org" . "\n";
-$headers .= 'Cc: ' . $CONF_REPORT_CC . "\n";
-$headers .= 'X-Mailer: PHP/' . phpversion() ."\n";
-$headers .= 'MIME-Version: 1.0' . "\n";
-$mime_boundary=md5(time());
-$headers .= "Content-Type: multipart/related; boundary=\"$mime_boundary\"\n";
+if($_REQUEST['gen'] == "email" || $_REQUEST['gen'] == "both") {
 
-$recipient = $CONF_REPORT_TO;
-$subject= "Attendee Report: " . date($SHORT_DATE_FORMAT,time());
+	// now mail this to someone
+	ini_set(SMTP, $MAIL_SERVER);
+	$headers  = 'From: ' . $HELP_EMAIL . "\n";
+	$headers .= 'Return-Path: ' . $HELP_EMAIL . "\n";
+	$headers .= 'Reply-To: ' . "donotreply@sakaiproject.org" . "\n";
+	$headers .= 'Cc: ' . $CONF_REPORT_CC . "\n";
+	$headers .= 'X-Mailer: PHP/' . phpversion() ."\n";
+	$headers .= 'MIME-Version: 1.0' . "\n";
+	$mime_boundary=md5(time());
+	$headers .= "Content-Type: multipart/related; boundary=\"$mime_boundary\"\n";
+	
+	$recipient = $CONF_REPORT_TO;
+	$subject= "Attendee Report: " . date($SHORT_DATE_FORMAT,time());
+	
+	// put in the attachment
+	$msg .= "--".$mime_boundary."\n";
+	$msg .= "Content-Type: text/x-csv; name=\"".$filename."\""."\n";
+	$msg .= "Content-Transfer-Encoding: base64"."\n";
+	$msg .= "Content-Disposition: attachment; filename=\"".$filename."\""."\n"."\n";
+	$msg .= base64_encode($fileContent)."\n"."\n";
+	
+	// put in a short message
+	$msg .= "--".$mime_boundary."\n";
+	$msg .= "Content-Type: text/plain; charset=\"iso-8859-1\""."\n";
+	$msg .= "Content-Transfer-Encoding: 8bit"."\n";
+	$msg .= "This mail is generated automatically by the sakaiproject.org website.\n" .
+		"This is a report of the current conference attendees for: $CONF_NAME ($CONF_ID).\n" .
+		"Report is attached as $filename and was generated: " . date($SHORT_DATE_FORMAT,time()) . "\n" .
+		"-- Sakai Webmaster\n";
+	$msg .= "--".$mime_boundary."--\n\n";
+	
+	//echo "Subject: $subject <br/>Message: $msg <br/>";
 
-// put in the attachment
-$msg .= "--".$mime_boundary."\n";
-$msg .= "Content-Type: text/x-csv; name=\"".$filename."\""."\n";
-$msg .= "Content-Transfer-Encoding: base64"."\n";
-$msg .= "Content-Disposition: attachment; filename=\"".$filename."\""."\n"."\n";
-$msg .= base64_encode($fileContent)."\n"."\n";
-
-// put in a short message
-$msg .= "--".$mime_boundary."\n";
-$msg .= "Content-Type: text/plain; charset=\"iso-8859-1\""."\n";
-$msg .= "Content-Transfer-Encoding: 8bit"."\n";
-$msg .= "This mail is generated automatically by the sakaiproject.org website.\n" .
-	"This is a report of the current conference attendees for: $CONF_NAME ($CONF_ID).\n" .
-	"Report is attached as $filename and was generated: " . date($SHORT_DATE_FORMAT,time()) . "\n" .
-	"-- Sakai Webmaster\n";
-$msg .= "--".$mime_boundary."--\n\n";
-
-//send the mail
-mail($recipient, $subject, $msg, $headers);
-
-//echo "Subject: $subject <br/>Message: $msg <br/>";
+	//send the mail
+	if(mail($recipient, $subject, $msg, $headers)) {
+		echo "Conf report email sent...<br/>";
+	} else {
+		echo "ERROR: Conf report email failure...<br/>";
+	}
+}
 ?>
