391419b865fb7f1bacfed62813da575289fba82f
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-04-24 21:49:40 +0000

    Wording updates
    git-svn-id: https://source.sakaiproject.org/www/trunk@701 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/skin/index.php
+++ Private/html/skin/index.php
@@ -33,22 +33,23 @@ $EXTRA_LINKS = " - <a style='font-size:.8em;' href='$HELP_LINK' target='_HELP'>H
 <div class="info">
 The <a href="http://bugs.sakaiproject.org/confluence/x/9yc">Default Skin WG</a> is coordinating a contest to
 create the next default skin for Sakai. <a href="skin_contest_rules.php">Complete details and rules</a> 
-for the contest are available online.<br>
-<br>
-<b>Contest Summary:</b><br>
-<b>Purpose</b><br>
+for the contest are available online.<br/>
+<br/>
+<b>Contest Summary:</b><br/>
+<b>Purpose</b><br/>
 The purpose of the contest is to solicit creative new ideas for a default skin with the goal of improving the 
 Sakai out-of-the box usability and visual appeal. The new default skin will be used in the Sakai 2.2 release. 
 The top contest entry will be included in the Sakai 2.2 bundle as the default skin. 
-The 2nd and 3rd place entries will be included in the enterprise bundle as options.<br>
-<br>
-<b>Process</b><br>
-<a href="submit.php">Submit your skin</a> by <?= date($DATE_FORMAT,strtotime($ROUND_CLOSE_DATE)) ?><br>
-<a href="vote.php">Cast votes</a> for best skins between <?= date($DATE_FORMAT,strtotime($ROUND_VOTE_DATE)) ?> and <?= date($DATE_FORMAT,strtotime($ROUND_END_DATE)) ?><br>
-Announce the winning skins at the <a href="http://www.sakaiproject.org/index.php?option=com_content&task=view&id=319&Itemid=527">Sakai Vancouver Conference<a/><br>
+The 2nd and 3rd place entries will be included in the enterprise bundle as options.<br/>
 <br/>
-<b>Contest Details:</b><br>
-<a href="skin_contest_rules.php">Complete details and rules for the contest are available here</a><br>
+<b>Process</b><br/>
+Create your skin using the <a href="http://bugs.sakaiproject.org/confluence/x/jzQ">Quickstart Sakai Package<a/><br/>
+<a href="submit.php">Submit your skin</a> by <?= date($DATE_FORMAT,strtotime($ROUND_CLOSE_DATE)) ?><br/>
+<a href="vote.php">Cast votes</a> for best skins between <?= date($DATE_FORMAT,strtotime($ROUND_VOTE_DATE)) ?> and <?= date($DATE_FORMAT,strtotime($ROUND_END_DATE)) ?><br/>
+Announce the winning skins at the <a href="http://www.sakaiproject.org/index.php?option=com_content&task=view&id=319&Itemid=527">Sakai Vancouver Conference<a/><br/>
+<br/>
+<b>Contest Details:</b><br/>
+<a href="skin_contest_rules.php">Complete details and rules for the contest are available here</a><br/>
 </div>
 </td>
 <td valign="top" width="25%">
