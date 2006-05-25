05838106ab7ce71c4615869dede6b3ebaa63c003
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-05-25 16:56:12 +0000

    Added conference links to accounts page
    git-svn-id: https://source.sakaiproject.org/www/trunk@853 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/accounts/index.php
+++ Private/html/accounts/index.php
@@ -37,6 +37,8 @@ You may access the following tools:<br/>
 This page allows you to create an account to access the following tools:<br/>
 <?php } ?>
 <a href="/conference/registration/">Conference Registration</a><br/>
+<a href="/conference/admin/schedule.php">Conference Schedule</a><br/>
+<a href="/conference/volunteer.php">Conference Volunteering</a><br/>
 <a href="/facebook/">Facebook</a><br/>
 <a href="/requirements/">Requirements Voting</a><br/>
 <a href="/skin/">Default Skin Submission and Voting</a><br/>
