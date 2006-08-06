de81d4bc9e15e1aba59eee3bcb4ee93f3fef71ce
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-08-06 18:09:10 +0000

    new template used for footer.php
    git-svn-id: https://source.sakaiproject.org/www/trunk@898 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/include/footer.php
+++ Private/html/conference/include/footer.php
@@ -6,29 +6,36 @@
  */
 ?>
 <!-- begin footerarea -->
-<div class="clear"></div>
-<div id="blackline"></div>
-	
-<!-- copyright notice -->
-<div id="copyright">
-copyright 2006
-<a href="http://sakaiproject.org/">Sakai Project</a> (<a href="http://sakaiproject.org/">http://sakaiproject.org/</a>) --
-<?= $TOOL_NAME ?> --
-<a href="http://bugs.sakaiproject.org/jira/">Sakai JIRA</a> --
-<a href="http://bugs.sakaiproject.org/confluence/">Sakai Confluence</a> -- 
-<a href="mailto:sakaiproject_webmaster@umich.edu">Contact Webmaster</a>
-<div style="font-size:8pt;text-align:center;">
-<!--end  copyright notice -->
-Coded by <a href="mailto:aaronz@vt.edu">Aaron Zeckoski</a> (<a href="http://www.vt.edu/">Virginia Tech</a>) and
-<a href="mailto:shardin@umich.edu">Susan Hardin</a> (<a href="http://www.umich.edu/">University of Michigan</a>)
+</td>
+                      </tr>
+                      <tr>
+                        <td width="100%"></td>
+                      </tr>
+                    </table></td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
-
-<!--end  footer -->
-<br/>&nbsp;<br/> <!-- a little extra whitespace -->
-
-</div><!-- end outerleft -->
-</div><!-- end container -->
-</div><!-- end center -->
-
+<div class="wrapper">
+  <div id="mainft-2">
+    <div id="mainft-3">
+      <div id="the-footer">
+        <div class="padding"> copyright 2006 <a href="http://sakaiproject.org/">Sakai Project</a> (<a href="http://sakaiproject.org/">http://sakaiproject.org/</a>) --
+            <?= $TOOL_NAME ?>
+            -- <a href="http://bugs.sakaiproject.org/jira/">Sakai JIRA</a> -- <a href="http://bugs.sakaiproject.org/confluence/">Sakai Confluence</a> -- <a href="mailto:sakaiproject_webmaster@umich.edu">Contact Webmaster</a>
+            <div style="font-size:8pt;text-align:center;">
+              <!--end  copyright notice -->
+              Coded by <a href="mailto:aaronz@vt.edu">Aaron Zeckoski</a> (<a href="http://www.vt.edu/">Virginia Tech</a>) and <a href="mailto:shardin@umich.edu">Susan Hardin</a> (<a href="http://www.umich.edu/">University of Michigan</a>) </div>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
