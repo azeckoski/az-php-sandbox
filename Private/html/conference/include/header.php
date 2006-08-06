b986d0ad2db666b0db7b6bbdac0fb0e238f84320
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-08-06 18:09:31 +0000

    new template used for header.php
    git-svn-id: https://source.sakaiproject.org/www/trunk@899 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/include/header.php
+++ Private/html/conference/include/header.php
@@ -1,122 +1,94 @@
 </head>
-<body class="waterbody">
-<div align="center">
-<!-- start centering -->
-<div id="container">
-<!-- start container -->
-<div id=header>
-  <!-- start header -->
-  <div id="logo">
-    <!-- start logo -->
-    <div id="logoimg">&nbsp;</div>
-    <div id=confbanner><a href="index.php"></a></div>
-    <div id=topSearch>
-      <table cellpadding="0" cellspacing="0" class="moduletable">
-        <tr>
-          <td> 
-              <div align="right" style="width:18em;padding:10px;background:#eee;font-size:12pt;color:#000;">
-<?php if ($User->pk > 0) { ?>
-	<span style="font-size:.8em;">
-  	Welcome,&nbsp;<?= $User->firstname."&nbsp;".$User->lastname ?>
-  	</span><br/>
-  	<a style="font-size:.7em;" href="<?= $ACCOUNTS_URL ?>/<?= $ACCOUNTS_PAGE ?>">My Account</a><br/>
-<?php if ($User->checkPerm("admin_conference")) { ?>
-  	<a style="font-size:.7em;" href="<?= $TOOL_URL ?>/admin/index.php">Tool Admin</a> - 
-<?php } ?>
-  	<a style="font-size:.7em;" href="<?= $ACCOUNTS_URL ?>/<?= $LOGOUT_PAGE ?>">Logout</a><br/>
-<?php } else { ?>
-  	<a style="font-size:.9em;" href="<?= $ACCOUNTS_URL ?>/<?= $LOGIN_PAGE."?ref=".$_SERVER['PHP_SELF'] ?>">Login</a><br/>		
-	<br/>
-<?php } ?>
-	<?= $EXTRA_MESSAGE ?>
-	<div style="margin:6px;"></div>
-	<a style="font-size:9pt;" href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=169&amp;Itemid=518">Conference Home</a> -
-	<a style="font-size:9pt;" href="<?= $TOOL_URL ?>/registration/">Registration</a> -
-	<a style="font-size:9pt;" href="<?= $TOOL_URL ?>/proposals/">Proposals</a>
-	<?= $EXTRA_LINKS ?>
+<body id="page_bg" class="w-wide f-default">
+<div id="mainbg">
+  <div class="wrapper">
+    <div id="mainbg-2">
+      <div id="mainbg-3">
+        <div id="mainbg-4">
+          <div id="mainbg-5">
+            <div id="header"> <a href="http://sakaiproject.org" title=""><span id="logo">&nbsp;</span></a>
+              <div id="top">
+                <table class="contentpaneopen">
+                  <tr>
+                    <td valign="top" colspan="2"><div align="right" style="padding:10px;background:#eee;font-size:12pt;color:#000;">
+                        <?php if ($User->pk > 0) { ?>
+                        <span style="font-size:.8em;"> Welcome,&nbsp;
+                        <?= $User->firstname."&nbsp;".$User->lastname ?>
+                        </span><br/>
+                        <a style="font-size:.7em;" href="<?= $ACCOUNTS_URL ?>/<?= $ACCOUNTS_PAGE ?>">My Account</a><br/>
+                        <?php if ($User->checkPerm("admin_conference")) { ?>
+                        <a style="font-size:.7em;" href="<?= $TOOL_URL ?>/admin/index.php">Tool Admin</a> -
+                        <?php } ?>
+                        <a style="font-size:.7em;" href="<?= $ACCOUNTS_URL ?>/<?= $LOGOUT_PAGE ?>">Logout</a><br/>
+                        <?php } else { ?>
+                        <a style="font-size:.9em;" href="<?= $ACCOUNTS_URL ?>/<?= $LOGIN_PAGE."?ref=".$_SERVER['PHP_SELF'] ?>">Login</a><br/>
+                        <br/>
+                        <?php } ?>
+                        <?= $EXTRA_MESSAGE ?>
+                     
+                        <a style="font-size:9pt;" href="http://www.sakaiproject.org/index.php?option=com_content&amp;amp;task=blogcategory&amp;amp;id=169&amp;amp;Itemid=518">Conference Home</a> - <a style="font-size:9pt;" href="<?= $TOOL_URL ?>/registration/">Registration</a> - <a style="font-size:9pt;" href="<?= $TOOL_URL ?>/proposals/">Proposals</a>
+                        <?= $EXTRA_LINKS ?>
+                      </div></td>
+                  </tr>
+                </table>
               </div>
-            </td>
-        </tr>
-      </table>
-    </div>
-  </div>
-  <!-- end logo -->
-</div>
-<!-- end header -->
-<div id="containerbg">
-<!-- start containerbg -->
-<div id="outerleft">
-<!-- start outerleft -->
-<!-- start top menu. -->
-<div id="topmenu" align=right> <a href="index.php">sakaiproject.org</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </div>
-<!-- end top menu.  -->
-<!-- start image header -->
-<!-- end image header -->
-<div id="container_inner">
-<!-- start left column. -->
-<div id="leftcol">
-  <!-- start left column. -->
-  <table cellpadding="0" cellspacing="0" class="moduletable">
-    <tr>
-      <th valign="top">Vancouver '06</th>
-    </tr>
-    <tr>
-      <td><table width="100%" border="0" cellpadding="0" cellspacing="0">
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=169&amp;Itemid=518" class="mainlevel" id="active_menu">Conference Home</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=170&amp;Itemid=519" class="mainlevel" >Call for Proposals</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=171&amp;Itemid=520" class="mainlevel" >Registration</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=174&amp;Itemid=521" class="mainlevel" >Program</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=173&amp;Itemid=523" class="mainlevel" >Technical Demos</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=178&amp;Itemid=524" class="mainlevel" >BOF Meetings</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=175&amp;Itemid=525" class="mainlevel" >Hotel Reservations</a></td>
-          </tr>
-          <tr align="left">
-            <td><a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=176&amp;Itemid=526" class="mainlevel" >About Vancouver</a></td>
-          </tr>
-          <tr align="left">
-            <td><span class="mainlevel" >&nbsp;</span></td>
-          </tr>
-        </table></td>
-    </tr>
-  </table>
-  <div id=activity>
-    <!-- start activity div -->
-<?php if ($_SESSION['firstname']) { ?>
-    <table cellpadding="0" cellspacing="0" class="moduletable">
-      <tr>
-        <th valign="top"> Proposal Activity</th>
-      </tr>
-      <tr>
-        <td align=left><strong>Proposal Activity for:</strong><br />
-          <?php $first=$_SESSION['firstname']; $last=$_SESSION['lastname'] ;  $num_pres=$_SESSION['num_pres'];
-    	         echo " $first $last<br /><br /><strong>Presentations: </strong>$num_pres"; ?></td>
-      </tr>
-      <tr>
-        <td align=left><?php $num_demo=$_SESSION['num_demo'];                                 
-			echo "<br /><strong>Demos: </strong>$num_demo"; ?>
-          <br />
-          <br /></td>
-      </tr>
-    </table>
-<?php } ?>
-  </div>
-  <!-- end activity div -->
-</div>
-<!-- end left column. -->
-<!-- start content top wrapper -->
-<!-- end content top wrapper -->
-<!-- start main body -->
-<div id="content_main">
+            </div>
+            <div id="showcase">
+              <div class="padding"> </div>
+            </div>
+            <div id="mainbody-padding">
+              <table class="mainbody" cellspacing="0">
+                <tr valign="top">
+                  <td class="left">
+                    <div class="moduletable">
+                     <div id=confLogo>
+                  	</div>
+                      <table width="100%" border="0" cellpadding="0" cellspacing="0">
+                        <tr align="left">
+                          <td><a href="http://shardin-mac.local/~shardin/sakaifoundation/index.php?option=com_frontpage&amp;Itemid=1" class="mainlevel" >Conference home</a></td>
+                        </tr>
+                        <tr align="left">
+                          <td><a href="http://shardin-mac.local/~shardin/sakaifoundation/index.php?option=com_content&amp;task=view&amp;id=5&amp;Itemid=6" class="mainlevel" >Help?</a></td>
+                        </tr>
+                        
+                        <tr><td>
+                        <?php 
+	$sql = "select CP.title, CP.pk from conf_proposals CP " .
+		"where CP.users_pk='$User->pk' and CP.confID = '$CONF_ID'";
+	//print "SQL=$sql<br/>";
+	$result = mysql_query($sql) or die("Query failed ($sql): " . mysql_error());
+
+	if(mysql_num_rows($result) > 0) {
+		// print the nice header
+?>
+	<div style="padding:15px;"></div><div style="padding:3px; margin:0px;border:1px solid #232833;background:white;">
+		<div><strong>Your&nbsp;Proposals:</strong></div>
+<?php
+	while($item=mysql_fetch_assoc($result)) {
+?>
+	<p><a href="edit_proposal.php?pk=<?= $item['pk'] ?>" title="Edit this proposal" ><?=  $item['title'] ?></a>
+		<!-- [<a style="color:red;" href="edit_proposal.php?pk=<?= $item['pk'] ?>&amp;delete=1" 
+			title="Delete this proposal"
+			onClick="return confirm('Are you sure you want to delete this proposal?');" >X</a>]  -->
+	</p>
+<?php
+		} // end while
+		echo "<hr/><p>[ <a title='Create a new proposal' href='index.php'>add a proposal</a> ]</p>";
+		echo "</div>";
+	} // end if
+?>
+
+                        
+                        </td>
+                        </tr>
+                      </table>
+                    </div>
+                    <div class="moduletable"> </div></td>
+                  <td class="mainbody" width="100%">
+                    <table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" class="contentpane">
+                      <tr>
+                        <td width="100%" valign="top" class="contentdescription" colspan="2">
+ 
+<div class="componentheading"><br/> 6th Sakai Conference - 
+                        December 5-8, 2006  -
+                        Atlanta, Georgia </div>
