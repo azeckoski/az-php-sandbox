f20cfc8457ad55c0cd8ba7428db580acdc442112
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-08-14 20:13:50 +0000

    added a left column menu specific to the conf_logos
    git-svn-id: https://source.sakaiproject.org/www/trunk@924 a679bb99-f00c-0410-8349-afe10950bba1

---
+++ Private/html/conference/conf_logos/include/contest_LeftCol.php
@@ -0,0 +1,57 @@
+<?php
+/*
+ * Created on Aug 14, 2006
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+?>
+         <div class="moduletable">
+                     <div id=confLogo>
+                  	</div>
+ 	<table cellpadding="0" cellspacing="0" class="moduletable">
+			<tr> <th>View Options</th> </tr>
+			<tr><td><br/>&nbsp;&nbsp;&nbsp;<strong>Display by:</strong>
+				<ul>
+				<li><a href="javascript:orderBy('date_created desc');">Recent Entry</a></li>
+				<li><a href="javascript:orderBy('lastname');">Last Name</a></li>
+				<li><a href="javascript:orderBy('institution');">Institution</a></li>
+				<li><a href="javascript:orderBy('date_modified desc');">Recent Update</a></li>
+				</ul>
+       		  </td></tr> 
+             
+             <tr><td>&nbsp;</td>
+           </tr>
+         </table>
+             
+             
+		<table cellpadding="0" cellspacing="0" class="moduletable">
+			<tr><th valign="top" align="left">&nbsp;&nbsp;&nbsp;Contest Entry</th></tr>
+			<tr><td>
+				<ul>
+                 <li> <a href="add_entry.php">Submit New Entry</a> </li>
+               	<li> <a href="add_entry.php">Edit Your Entry</a><br/> </li>
+               </ul>
+               	
+              </td>
+			</tr>
+				
+			<tr><td valign=top>
+            <br /><div id=help>
+			<p><strong>Need help?</strong><br />If you experience problems adding or editing your logo or theme, 
+			please contact: 
+			 <a style="color: #006699;" href="mailto:<?= $HELP_EMAIL ?>">webmaster</a> .
+			<!-- <a style="color: #006699;" href="mailto:<?= $HELP_EMAIL ?>"><?= $HELP_EMAIL ?></a> -->
+		   </p></div>
+			</td></tr>
+		</table>
+	              </div>
+                    <div class="moduletable"> </div></td>
+                  <td class="mainbody" width="100%">
+                    <table width="100%" cellpadding="0" cellspacing="0" border="0" align="center" class="contentpane">
+                      <tr>
+                        <td width="100%" valign="top" class="contentdescription" colspan="2">
+ 
+<div class="componentheading"><br/> 6th Sakai Conference - 
+                        December 5-8, 2006  -
+                        Atlanta, Georgia </div>
