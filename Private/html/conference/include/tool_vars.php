d9cc7e4fe6b18e9a404754cec06851f8a78e6565
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-08-25 12:07:29 +0000

    set voting date for proposals to Sept 15 2006 - October 5 2006
    git-svn-id: https://source.sakaiproject.org/www/trunk@955 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/include/tool_vars.php
+++ Private/html/conference/include/tool_vars.php
@@ -46,7 +46,7 @@ $VOTE_HELP = array(
 $VOTE_SCORE = array("2", "1", "0");
 
 // dates below in format: YYYY/MM/DD HH24:MI
-$VOTE_OPEN_DATE = "2006/09/1 8:00";
+$VOTE_OPEN_DATE = "2006/09/15 8:00";
 $VOTE_CLOSE_DATE = "2006/10/5 5:00";
 
 // conference attendee reports
