895dc1e9c3db9c8b950fd45729b5e8f8522fa26b
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-09-03 22:08:33 +0000

    added auth message
    git-svn-id: https://source.sakaiproject.org/www/trunk@1006 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/logos/add_entry.php
+++ Private/html/conference/logos/add_entry.php
@@ -17,6 +17,10 @@ require 'sql/mysqlconnect.php';
 // check authentication
 require $ACCOUNTS_PATH.'include/check_authentic.php';
 
+
+// login if not autheticated
+$AUTH_MESSAGE = "You must login to submit a logo or theme for the $CONF_NAME conference. If you do not have an account, please create one.";
+
 // login if not autheticated
 require $ACCOUNTS_PATH.'include/auth_login_redirect.php';
 
