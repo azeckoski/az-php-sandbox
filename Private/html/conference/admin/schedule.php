cb3d21fca8384361bdae7a533a76226be4bc0c7f
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-11-11 15:05:52 +0000

    git-svn-id: https://source.sakaiproject.org/www/trunk@1147 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/admin/schedule.php
+++ Private/html/conference/admin/schedule.php
@@ -20,6 +20,9 @@ require $ACCOUNTS_PATH.'include/check_authentic.php';
 // login if not autheticated - not required
 //require $ACCOUNTS_PATH.'include/auth_login_redirect.php';
 
+//comment testing strange behavior in updating on live server
+
+
 // THIS PAGE IS ACCESSIBLE BY ANYONE
 // Make sure user is authorized for admin perms
 $isAdmin = false; // assume user is NOT allowed unless otherwise shown
