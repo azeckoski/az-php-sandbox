2b5a7ff68a21ae643421193f2fc988f050d68af8
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2006-11-30 19:58:42 +0000

    corrected use of images for printed schedule
    git-svn-id: https://source.sakaiproject.org/www/trunk@1171 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/admin/schedulePrint.php
+++ Private/html/conference/admin/schedulePrint.php
@@ -346,7 +346,7 @@ if ($proposal['type']=='BOF') { continue; }
 							case "OSP": $image_file = "ospiNEWicon.jpg' width=14"; break;
 							case "Cool New Tools": $image_file = "coolToolicon.gif' height=14 width=14"; break;
 							case "Cool Commercial Tool": $image_file = "coolCommercialToolicon.gif' height=14 width=14"; break;
-							case "User Experience": $image_file = "people_Icon.jpg' height=14 width=14 "; break;
+							case "User Experience": $image_file = "people_icon.jpg' height=14 width=14 "; break;
 							case "Library": $image_file = "book06.gif' height=14 width=14"; break;
 							
 	}
