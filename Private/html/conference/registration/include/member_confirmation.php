979ac7583eed071957d68194ba0bc8bf57fba0c6
shardin@umich.edu <shardin@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2007-03-12 13:37:27 +0000

    updated text and layout on memberconfirmation.php
    git-svn-id: https://source.sakaiproject.org/www/trunk@1211 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/conference/registration/include/member_confirmation.php
+++ Private/html/conference/registration/include/member_confirmation.php
@@ -1,52 +1,79 @@
 
 <table width="500"  id=confirm  cellpadding="3" cellspacing="0">
-  <tr>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td  colspan=2 style=" padding:5px;">
+ 
+ <?php if ($SEND_EMAIL) { ?>
+ 	 <tr>
+    <td  colspan=3 style=" padding:5px;">
     	<strong>Registration Complete</strong>: <br/>
-      Thank you for registering for the Sakai Atlanta conference.
-<?php if ($SEND_EMAIL) { echo "You will receive a confirmation email shortly."; } ?>
-      We look forward to seeing you in Atlanta! <br/>
-      <br/>
-      -- Sakai Conference Committee
+      Thank you for registering for the Sakai Amsterdam conference.
+ 	 You will receive a confirmation email shortly.
+   
     </td>
-</tr>
+</tr> <?php } ?>
 <tr>
-	<td valign="top" align="left">
-		<strong>Registrant's Information: </strong>
-		<br/>
-		<strong>name:</strong> <?= $User->firstname."&nbsp;".$User->lastname ?><br/> 
-		<strong>email: </strong> <?= $User->email ?><br/>
-		<strong>institution: </strong> <?= $User->institution ?><br/>
-		<strong>address:</strong><br/>
-		<?= nl2br($User->address) ?><br/>
-		<?= $User->city ." ". $User->state .", ". $User->zipcode ?><br/>
-		<?= $User->country ?><br/>
-		<br />
-		<strong>phone:</strong> <?= $User->phone ?><br/>
+		<td valign=top colspan=3 style="border-bottom: 0px solid #cccccc; width:100%; font-size:12px;"><span style="font-weight:bold; font-size:1.2em">Your Registration Details <br/><br/></span></td>
+	</tr>
+	<tr>
+		<td width="10%" valign=top style="padding-right:10px;border-right: 1px dotted #cccccc;" ><strong>Contact Information</strong></td>
+		<td valign=top width="30%" style="padding-left:10px">
+			<?= $User->firstname."&nbsp;".$User->lastname ?><br/> 
+			<?= $User->institution ?><br/>
+			<?= nl2br($User->address) ?><br/>
+			<?= $User->city ." ". $User->state .", ". $User->zipcode ?><br/>
+			<?= $User->country ?><br/>
+			<br />
+		</td>
+		<td valign=top >	<strong>email: </strong> <?= $User->email ?><br/>
+			<strong>phone: </strong>  <?= $User->phone ?>
 		<?php if ($User->fax) { ?>
-		<strong>fax:</strong> <?= $User->fax ?><br/>
+		<strong>fax: </strong> <?= $User->fax ?><br/>
 		<?php } ?>
-		<br/>
-		<strong>shirt size:</strong> <?= $CONF['shirt'] ?><br/>
-		<strong>attending jasig:</strong> <?= $CONF['jasig'] ?><br/>
-		<strong>staying at conference hotel:</strong> <?= $CONF['confHotel'] ?><br/>
-		<strong>publish name on attendee list:</strong> <?= $CONF['publishInfo'] ?><br/>
-		<?php if ($CONF['special']) { echo "<strong>Special needs: </strong> $CONF[special]<br/>"; } ?>
-		<?php if ($CONF['expectations']) { echo "<strong>Expectations:</strong> $CONF[expectations]<br/>"; } ?>
-		<?php if ($CONF['attending']) { echo "<strong>Attending: </strong> $CONF[attending]<br/>"; } ?>
-		<br/>
-	</td>
-</tr>
-<!-- <tr>
-    <td colspan=2><blockquote style="background:#fff; border: 1px solid #ffcc33; padding: 5px;">
-    <strong>Special announcements and reminders:</strong>
-        <ul>
-          <li><strong>Visit the Sakai Conference Facebook</strong> to see who else is attending,  and add your photo while you're there! (see sidebar for more information)</li>
-          <li><strong>Call for Proposals Deadline is March 31st.</strong> [ <a href="http://www.sakaiproject.org/index.php?option=com_content&amp;task=blogcategory&amp;id=170&amp;Itemid=519">more information</a> ]</li>
-        </ul>
-      </blockquote></td>
-  </tr> -->
-</table>
+	
+		</td>
+	</tr>
+	<tr><td>&nbsp;</td></tr>
+	<tr>
+		<td valign=top style="border-right: 1px dotted #cccccc; padding-right:10px;"><strong>Registration Information</strong></td>
+		<td colspan=2 style="padding-left:10px;">
+				<strong>Date Registered: </strong> &nbsp;&nbsp;&nbsp;&nbsp; <?= $CONF['date_created'] ?><br/>
+				<strong>T-shirt size:</strong> &nbsp;&nbsp;&nbsp;&nbsp; <?= $CONF['shirt'] ?><br/>
+				
+					<?php if ($CONF['jasig']) { ?>
+						<strong>Attending Jasig:</strong> &nbsp; &nbsp;&nbsp;&nbsp;<?= $CONF['jasig'] ?><br/>
+					
+					<?php }?>
+					
+						<strong>Staying at conference hotel:</strong> &nbsp;&nbsp;&nbsp;&nbsp; <?= $CONF['confHotel'] ?><br/>
+					 
+					
+						<strong>Name on publidhed attendee list:</strong> &nbsp;&nbsp;&nbsp;&nbsp; <?= $CONF['publishInfo'] ?><br/>
+					
+					<?php if ($CONF['special']) { ?>
+					<strong>Special needs: </strong>&nbsp;&nbsp;&nbsp;&nbsp;<?=$CONF['special']?><br/>
+					<?php } ?>
+						<?php if ($CONF['expectations']) { ?>
+						<strong>Expectations:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<?=$CONF['expectations']?><br/>
+					<?php } ?>	<?php if ($CONF['attending']) { ?>
+					<strong>Dates Attending: </strong> &nbsp;&nbsp;&nbsp;&nbsp;<?=$CONF['attending']?><br/>
+					<?php } ?>		
+				
+					
+				
+					
+  	</tr>
+  	
+	<tr><td>&nbsp;</td></tr>
+	<tr>
+		<td valign=top style="border-right: 1px dotted #cccccc; padding-right:10px;"><strong>Payment Information</strong><br/><br/><br/></td>
+		<td colspan=2 style="padding-left:10px;"><br/>
+		<strong>Registration Fee</strong>:  &nbsp;&nbsp;&nbsp;&nbsp;
+						<?php if (!$amount) {
+							$amount="No Fee Required ";  echo $amount; } 
+						else { 
+							echo "$" .$amount;  ?><br/>
+						<strong>Amount Paid</strong>:  &nbsp;&nbsp;&nbsp;&nbsp; $<?=$amount?>
+						<?php } ?>					
+
+</td></tr>
+  	<tr><td><div class="padding50"> </div>
+	</table>
