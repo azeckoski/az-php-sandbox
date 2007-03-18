b0899cbd8b7f1d764166ab5bd42b2dc53385b71d
aaronz@vt.edu <aaronz@vt.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2007-03-18 17:15:57 +0000

    Removed secure and backup directories -AZ
    git-svn-id: https://source.sakaiproject.org/www/trunk@1231 a679bb99-f00c-0410-8349-afe10950bba1

--- Private/html/accounts/include/system_vars.php
+++ Private/html/accounts/include/system_vars.php
@@ -6,7 +6,7 @@
 // You should change the environment variable to correspond to the environment you're using.
 
 // supported options are "dev","test", and "prod"
-$ENVIRONMENT = "devSusan";
+$ENVIRONMENT = "test";
 
 if ($ENVIRONMENT == "dev") {
 	// Set needed system variables
