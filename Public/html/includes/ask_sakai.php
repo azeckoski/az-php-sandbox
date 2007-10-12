65ee85dbd93044fce849c28bd12eb835037b6dbb
arwhyte@umich.edu <arwhyte@umich.edu@a679bb99-f00c-0410-8349-afe10950bba1>
2007-10-12 18:30:54 +0000

    Route mail to arwhyte during mmiles convalesence.
    git-svn-id: https://source.sakaiproject.org/www/trunk@1471 a679bb99-f00c-0410-8349-afe10950bba1

--- Public/html/includes/ask_sakai.php
+++ Public/html/includes/ask_sakai.php
@@ -1,56 +1,70 @@
 <?php 
+
 if (isset($_POST['submit'])){
 
-      error_reporting(0);
 	  $errors="";
       // initialize a variable to 
 	  
       //hold any errors we encounter
       $error_msg = "";
-      // test to see if the form was actually 
-      // check to see if requiired fields were entered
+      $email=$_POST['email'];
+	 
+    
+    // check to see if required fields were entered
   
-
-
-  if (!$_POST['Name'])
-         $error_msg .= "&nbsp; &nbsp; *Name";
-		 
-      if (!$_POST['customfield_10021'])
-         $error_msg .= "&nbsp; &nbsp; *Email";
-		 
-      if (!$_POST['summary'])
-         $error_msg .= "&nbsp; &nbsp; *Subject";
-		 
-      if (!$_POST['Institute'])
-         $error_msg .= "&nbsp; &nbsp; *Institution or Organization";
+  if (($_POST['Name']) && ($_POST['email']) && ($_POST['Institute']) 
+  		&& ($_POST['components']) && ($_POST['description']) && ($_POST['summary'])) {
+    
+	  if(!eregi("^[a-z0-9]+([_\\.-][a-z0-9]+)*" ."@"."([a-z0-9]+([\.-][a-z0-9]+)*)+"."\\.[a-z]{2,}"."$",$email))
+		 {
+ 			  $msg.="Invalid e-mail address.";
+ 			  $error++;
+			}
+         }  else {   
+           
+           $error++;
+	
+         $msg.="<p><strong>Please complete all required fields below:</strong></p>";
+		}
          
-      if ($_POST['components'] < '0')
-         $error_msg .= "&nbsp; &nbsp; *Category";
-         
-	 if (!$_POST['description'])
-         $error_msg .= "&nbsp; &nbsp; *Question or comment";
-		 		 
-		 
+         if (!$errors) {
+          //all required fields include data, now check for possible spam
+      
+        if(eregi("(@|href=)",$_POST['Name'])) {
+			 $msg.="Name field cannot contain an email address<br />";
+		   $error++;
+		}  
+		if(eregi("(@|href=)",$_POST['Institute']))  {
+		  $msg.="Organization field cannot contain an email address<br />";
+		   $error++;
+		} 
+		if (eregi("(@|href=)",$_POST['summary'])) { 
+			$msg.="Subject contains invalid characters<br />";
+		   $error++;
+		}
+		if(eregi("(href=)",$_POST['description'])) {
+ 			$msg.="invalid characters in comment (no html allowed)<br />";
+		  $error++;
+		}
+     
+    }
    	  // if there are any errors, display them
-      if ($error_msg) {
-   		  echo "<p><strong>Please complete all required fields below:</strong></p>";
+      if ($error) {
+   		  echo "<p style='color:red'>$msg</p>";
+	
 		  
-		  $email=$_POST['customfield_10021'];
-		  
-		}  else {     
+		}  else {  
 
 require_once('mysqlconnectWeb.php');
-
-
 
 $query="INSERT INTO ask_sakai values('', NOW(), '$_POST[issuetype]', '$_POST[Name]',
 '$_POST[Institute]',
 '$_POST[Phone]',
-'$_POST[customfield_10021]', 
+'$_POST[email]', 
 '$_POST[summary]', '$_POST[components]', '$_POST[description]'
 )";
 	$result = mysql_query($query) or die(mysql_error("There was a 
-	problem with the submission form submission.  Please try again or email the sakaiproject.org webmaster at shardin@umich.edu
+	problem with the submission form submission.  Please try again or email the sakaiproject.org webmaster at sakaiproject_webmaster@umich.edu
 		"));
 
 $id = mysql_insert_id(); //get this proposal_pk
@@ -62,77 +76,55 @@ echo "<p><strong>Your request was successfully submitted. </strong></p>
 <p>Thank you for your interest in the Sakai Project.</p>";
 echo "<br /><br /><br /><br /><br /><br />";
 
-
-
-
 //set up the email confirmations
 
 	// $msg ="Please do not reply to this email.  Someone will be in touch with you soon.\r\n\r\n";
-								
-			
+		
 			$msg="Name:   $_POST[Name]\r\n";
 			$msg.="Institute/Org:   $_POST[Institute]\r\n";
-			$msg.="Email:   $_POST[customfield_10021] \r\n";
+			$msg.="Email:   $_POST[email] \r\n";
 			$msg.="Category:   $_POST[components] \r\n\r\n";
 
 			$msg.="Subject:   $_POST[summary] \r\n";
 			$msg.="Question or Comment:\r\n   $_POST[description] \r\n";
-			
-			
-	$recipient="mmiles@umich.edu";
+					
+	//$recipient="mmiles@umich.edu mkorcuska@sakaifoundation.org";
+	$recipient="arwhyte@umich.edu mkorcuska@sakaifoundation.org";
 
 	if ($_POST['components'] == 'Tech')
-	$recipient="arwhyte@umich.edu";
+	$recipient="arwhyte@umich.edu mkorcuska@sakaifoundation.org";
 	
 	if ($_POST['components'] == 'SPOT')
-	$recipient="arwhyte@umich.edu mwagner@umich.edu";
+	$recipient="arwhyte@umich.edu mwagner@umich.edu mkorcuska@sakaifoundation.org";
 	
-if ($_POST['components'] == 'Web')
- $recipient="shardin@umich.edu";
+	if ($_POST['components'] == 'Web')
+ 	$recipient="arwhyte@umich.edu mkorcuska@sakaifoundation.org";
 
-
-
-
-	
 			//set up mail for Sakai Staff
 			$subject= "Ask Sakai: $_POST[summary]";
 			$mailheaders = "Content-type: text/plain; charset=ISO-8859-1\r\n";
-			$mailheaders .="From: www.sakaiproject.org \n";
-			$mailheaders .="Reply-To: $_POST[customfield_10021]";
+			$mailheaders .="From: sakaiproject_webmaster@umich.edu \n";
+			$mailheaders .="Reply-To: $_POST[email]";
 
 			//send the mail
 			mail($recipient, $subject, $msg, $mailheaders);
-			
-			
-			 $recipient="shardin@umich.edu";
-
-			$subject= "COPY: $_POST[summary]";
-			$mailheaders = "Content-type: text/plain; charset=ISO-8859-1\r\n";
-			$mailheaders .="From: www.sakaiproject.org \n";
-			$mailheaders .="Reply-To: $_POST[customfield_10021]";
-
-			//send the mail
-			mail($recipient, $subject, $msg, $mailheaders);
-			
-//backup copy to anthony
-
+				
+			// backup copy to anthony
 			$recipient="arwhyte@umich.edu";
 
 			$subject= "COPY: $_POST[summary]";
 			$mailheaders = "Content-type: text/plain; charset=ISO-8859-1\r\n";
 			$mailheaders .="From: www.sakaiproject.org \n";
-			$mailheaders .="Reply-To: $_POST[customfield_10021]";
+			$mailheaders .="Reply-To: $_POST[email]";
 
-			//send the mail
-		//	mail($recipient, $subject, $msg, $mailheaders);
-			
-			
+			// send the mail
+			// mail($recipient, $subject, $msg, $mailheaders);
+	
 }
 }
 }
 		  
 ?>
-
 
 <?php if (!$id) {
 //not successfully sent or a new form request
@@ -145,15 +137,12 @@ if ($_POST['components'] == 'Web')
  <br>
  </td></tr><tr><td valign="top" align="right" class="fieldLabelArea"><em><span title="Fields in italics are required"> <sup>*</sup> Name</span></em> </td><td nowrap="nowrap" class="fieldValueArea">
 <input type="text" name="Name" size="40" maxlength="150" value="<?php echo $_POST['Name']; ?>"> </td></tr><tr><td valign="top" align="right"> <span class="label"> <em><span title="Fields in italics are required"> <sup>*</sup> E-mail : </span></em></span></td><td nowrap="nowrap" class="fieldValueArea">
-<input type="text" name="customfield_10021" maxlength="150" value="<?php echo $email; ?>"> <br>
+<input type="text" name="email" maxlength="150" value="<?php echo $email; ?>"> <br>
  </td></tr><tr><td valign="top" align="right" class="fieldLabelArea"> <em><span title="Fields in italics are required"> <sup>*</sup> Organization</span></em> </td><td nowrap="nowrap" class="fieldValueArea">
-<input type="text" name="Institute" size="40" maxlength="150" value="<?php echo $_POST['Institute']; ?>"> </td></tr><tr><td valign="top" align="right" class="fieldLabelArea"><em><span title="Fields in italics are required">&nbsp;Phone</span></em> </td><td nowrap="nowrap" class="fieldValueArea">
-<input type="text" name="Phone" size="20" maxlength="20" value="<?php echo $_POST['Phone']; ?>"> </td></tr><tr><td valign="top" colspan="1">
-<p>&nbsp;</p></td><td valign="top" colspan="1">
-<p>&nbsp;</p></td></tr><tr><td valign="top" align="right" class="fieldLabelArea"><em><span title="Fields in italics are required">
+<input type="text" name="Institute" size="40" maxlength="150" value="<?php echo $_POST['Institute']; ?>"> </td></tr><tr><td valign="top" align="right" class="fieldLabelArea"><em><span title="Fields in italics are required">
 <sup>*</sup>Select a category</span></em>: </td><td nowrap="nowrap" class="fieldValueArea"><select name="components">
-<option value="-1">---Select---</option>
-<option value="SEPP"> Sakai Educational Partners Program </option>
+<option value="">---Select---</option>
+<option value="SEPP"> Sakai Foundation Partners Program </option>
 <option value="SCA"> Sakai Commercial Affiliates </option>
 <option value="SPOT"> Sakai Spotlight or Implementations </option>
 <option value="Sakai"> Sakai Project- general </option>
